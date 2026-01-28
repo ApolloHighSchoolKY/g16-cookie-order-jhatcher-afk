ffb44327a9c98a2b0e7b22989efcaeca0ae68542
jhatcher-afk <jaxon.hatcher@stu.daviess.kyschools.us>
2026-01-28 18:45:52 +0000

    commit

--- MasterOrder.java
+++ MasterOrder.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class MasterOrder{
 
   /** The list of all cookie order */
@@ -17,7 +19,14 @@ public class MasterOrder{
   public int getTotalBoxes()
   {
     /* Part A answer goes here */
-    
+    int sum = 0
+
+    for(CookieOrder co: orders)
+    {
+      sum += co.getNumBoxes();
+    }
+
+    return sum;
   }
 
   /** Removes all cookie orders from the master order that have the same variety of
@@ -28,7 +37,17 @@ public class MasterOrder{
   public int removeVariety(String cookieVar)
   {
     /* Part B answer goes here */
-    
+    int sum = 0;
+
+    for(int x = orders.size();x>=0;x--)
+    {
+      if(orders[x].getVariety()== cookieVar)
+      {
+         sum += x 
+         orders.remove(x);
+      }
+      return sum;
+    }
   }
 
   //There may be instance variables, constructors, and methods that are not shown.
